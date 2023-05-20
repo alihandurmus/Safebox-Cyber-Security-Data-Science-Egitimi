6180b9ae78f29a2d265378edd800ccfe923ae899
Alihan Durmuş <98883529+alihandurmus@users.noreply.github.com>
2023-05-20 14:23:41 +0300

    Update arama.py

--- odev2/arama.py
+++ odev2/arama.py
@@ -1,13 +1,13 @@
 
 
-def filmarama():
+def filmarama():#filmi ismiyle arayıp bilgileri çıkaran fonksiyon
     isim = input("Arayacağınız film adını giriniz:")
     with open("filmler.txt","r") as f:
         for satir in f:
             bilgiler = satir.split(":")
 
             ad = bilgiler[0]
-            if ad.lower() == isim.lower():
+            if ad.lower() == isim.lower():#kullanıcının girdiği isim ile dosyada bulunan isim aynı ise ife girer ve diğer bilgileri yazdırır.
                 print("Bulundu")
                 print("Film ismi:"+bilgiler[0])
                 print("Yapim yili:"+bilgiler[1])
