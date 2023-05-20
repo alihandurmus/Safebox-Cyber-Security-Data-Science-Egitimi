8dbfbf86e140c560f3f1c36340fe70bbdf3ce22c
Alihan Durmuş <98883529+alihandurmus@users.noreply.github.com>
2023-05-20 20:33:11 +0300

    Update filmekleme.py

--- odev2/filmekleme.py
+++ odev2/filmekleme.py
@@ -4,5 +4,5 @@ def ekle(film=film):
     film1 = film.obj()
 
     with open("filmler.txt","a") as f:
-        f.write(film1.isim+":"+film1.yil+":"+film1.yonetmen+":"+film1.tur + "\n")
+        f.write(film1.isim+":"+film1.yil+":"+film1.yonetmen+":"+film1.tur + "\n")#filmi burada dosyaya ekleme.
     print("Film eklendi")
