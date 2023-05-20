7850a479c79a361dd6f4587b292298c9d23ab305
Alihan Durmuş <98883529+alihandurmus@users.noreply.github.com>
2023-05-20 12:07:13 +0300

    Update film.py

--- odev2/film.py
+++ odev2/film.py
@@ -1,16 +1,16 @@
-class Film():
+class Film():#film sınıfı oluşturma
     def __init__(self,isim,yil,yonetmen,tur):
-        self.isim = isim
+        self.isim = isim#özelliklerini belirleme
         self.yil = yil
         self.yonetmen = yonetmen
         self.tur = tur
 
-def obj():
-    isim = input("Film ismi giriniz:")
+def obj():#film sınıfı için nesne oluşturma fonskiyonu
+    isim = input("Film ismi giriniz:")#özellikleri için kullanıcıdan değer alma
     yil = input("Yapim yilini giriniz:")
     yonetmen = input("Yonetmen ismini giriniz:")
     tur = input("Film turunu giriniz:")
 
-    film1 = Film(isim, yil, yonetmen, tur)
+    film1 = Film(isim, yil, yonetmen, tur)#film sınıfı için nesne oluşturma kısmı
 
     return film1
