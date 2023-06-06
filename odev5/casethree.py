86cce995b43732022a16482a0703df1dfd5d14a5
Alihan Durmuş <98883529+alihandurmus@users.noreply.github.com>
2023-06-06 23:59:28 +0300

    Update casethree.py

--- odev5/casethree.py
+++ odev5/casethree.py
@@ -1,18 +1,23 @@
-from flask import Flask, request, jsonify,render_template,session,redirect,url_for,send_from_directory
+from flask import Flask, request,jsonify ,render_template,session,redirect,url_for,send_from_directory
 from pymongo import MongoClient
 app = Flask(__name__)
 host = "localhost"
-port= 27017
+port= 5000
 username = "alihandurmus1907"
 password = "aF2QbJmfM65bZgkW"
 database = 'flaskmongodb'
 
-connection = MongoClient(host,port,username=username,password=password)
+connection = MongoClient("mongodb+srv://alihandurmus1907:aF2QbJmfM65bZgkW@flaskmongodb.zt7t68i.mongodb.net/")
 db = connection[database]
 UsersCollection = db['Users']
 @app.route("/",methods=["GET"])
-def home():
-    return render_template("index.html")
+def get_all_user():
+    users = UsersCollection.find({})
+    user_list = []
+    for user in users:
+        user_list.append(str(user))
+
+    return render_template("index.html") + str(jsonify(user_list))
 
 @app.route("/echo/<text>")
 def repeat(text):
@@ -29,21 +34,14 @@ def get_users_over_25():
     for user in users:
         user_list.append(user)
     return jsonify(user_list)
-@app.route('/',methods=['GET'])
-def get_all_user():
-    users = UsersCollection.find({})
-    user_list = []
-    for user in users:
-        user_list.append(user)
-    return jsonify(user_list),200
+
 @app.route('/deleteuser',methods=['POST','DELETE'])
 def delete_user():
     user_id = request.args.get('id')
     UsersCollection.delete_one({'_id':user_id})
     return jsonify({'message':'Kullanıcı başarıyla silindi'})
 if __name__=="__main__":
-    app.run(host='localhost',port=5000)
-
+    app.run()
 
 
 
